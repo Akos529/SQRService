31a18602d0895f3c10656c147b3a11be400e5c85
Taisia Ivanovskaya <taya.naumova@gmail.com>
2022-05-17 16:10:55 +0300

    Tests added

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,10 +2,10 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int sqrCount (int lowerLimit, int upperLimit){
+    public int sqrCount(int lowerLimit, int upperLimit) {
         int count = 0;
-        for (int i = 10; i <= 99; i++){
-            if (lowerLimit <= i*i && i*i <= upperLimit){
+        for (int i = 10; i <= 99; i++) {
+            if (lowerLimit <= i * i && i * i <= upperLimit) {
                 count++;
             }
         }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SQRServiceTest {
 
     @Test
-    void shouldCalculateSQR() {
+    public void shouldCalculateNumberOfSQRInRange() {
         SQRService service = new SQRService();
         int lowerLimit = 200;
         int upperLimit = 300;
@@ -17,4 +17,47 @@ public class SQRServiceTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldCalculateNumberOfSQRInZeroRange() {
+        SQRService service = new SQRService();
+        int lowerLimit = 200;
+        int upperLimit = 200;
+        int expected = 0;
+
+        int actual = service.sqrCount(lowerLimit, upperLimit);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateNumberOfSQRWhenLowerLimitSameAsSQR() {
+        SQRService service = new SQRService();
+        int lowerLimit = 100;
+        int upperLimit = 150;
+        int expected = 3;
+
+        int actual = service.sqrCount(lowerLimit, upperLimit);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateNumberOfSQRWhenUpperLimitSameAsSQR() {
+        SQRService service = new SQRService();
+        int lowerLimit = 101;
+        int upperLimit = 144;
+        int expected = 2;
+
+        int actual = service.sqrCount(lowerLimit, upperLimit);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateNumberOfSQRRangeUpsideDown() {
+        SQRService service = new SQRService();
+        int lowerLimit = 150;
+        int upperLimit = 100;
+        int expected = 0;
+
+        int actual = service.sqrCount(lowerLimit, upperLimit);
+        assertEquals(expected, actual);
+    }
 }
